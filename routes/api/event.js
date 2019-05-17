e87045dee5754cfeae7f89348845058f9d07a509
Ivan Naluzhnyi <ivan.naluzhnyi@gmail.com>
2019-05-17 19:12:20 +0200

    adsqfsds

--- routes/api/event.js
+++ routes/api/event.js
@@ -3,11 +3,7 @@ const bodyParser = require('body-parser');
 const router = express.Router();
 const eventController = require('../../controllers/eventController');
 
-router.post(
-  '/location/client',
-  bodyParser.json(),
-  eventController.sendEventsLocationClient
-);
+router.get('/location/client', eventController.sendEventsLocationClient);
 
 // router.route('/location/client').post(eventController.sendEventsLocationClient);
 router.get('/artist', eventController.sendUpcomingEventsByArtistName);
