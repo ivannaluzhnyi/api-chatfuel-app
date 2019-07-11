e81171636e50483bd3f6bbcec6a0849f00b9935a
Ivan Naluzhnyi <ivan.naluzhnyi@gmail.com>
2019-07-11 18:38:35 +0200

    test

--- controllers/artistController.js
+++ controllers/artistController.js
@@ -2,88 +2,87 @@ const fetchJson = require("../services/call-api");
 
 exports.sendArtistsByName = (req, res) => {
   const prepareDataToSend = (response) => {
+    // const prepare = {
+    //   set_attributes: {
+    //     artist_search_events: req.query.q
+    //   },
+    //   messages: [
+    //     {
+    //       attachment: {
+    //         type: 'template',
+    //         payload: {
+    //           template_type: 'generic',
+    //           image_aspect_ratio: 'square',
+    //           elements: []
+    //         }
+    //       }
+    //     }
+    //   ]
+    // };
+
+    // if (response.error) {
+    //   return response.error;
+    // }
+
+    // response.data.forEach(item => {
+    //   prepare.messages[0].attachment.payload.elements.push({
+    //     title: item.name,
+    //     image_url: item.picture_xl,
+    //     subtitle: 'Size: M',
+    //     buttons: [
+    //       {
+    //         type: 'web_url',
+    //         url: item.link,
+    //         title: 'Voir sur deezer'
+    //       }
+    //     ]
+    //   });
+    // });
+
     const prepare = {
-      set_attributes: {
-        artist_search_events: req.query.q
-      },
       messages: [
         {
           attachment: {
-            type: "template",
+            type: "image",
             payload: {
-              template_type: "generic",
-              image_aspect_ratio: "square",
-              elements: []
+              url: "https://rockets.chatfuel.com/assets/welcome.png"
             }
           }
         }
       ]
     };
 
-    if (response.error) {
-      return response.error;
-    }
-
-    response.data.forEach((item) => {
-      prepare.messages[0].attachment.payload.elements.push({
-        title: item.name,
-        image_url: item.picture_xl,
-        subtitle: "Size: M",
-        buttons: [
-          {
-            type: "web_url",
-            url: item.link,
-            title: "Voir sur deezer"
-          }
-        ]
-      });
-    });
-
     // return prepare;
     return {
-      messages: [
+      "messages": [
         {
-          attachment: {
-            type: "template",
-            payload: {
-              template_type: "generic",
-              image_aspect_ratio: "square",
-              elements: [
+          "attachment": {
+            "type": "template",
+            "payload": {
+              "template_type": "button",
+              "text": "Hello!",
+              "buttons": [
                 {
-                  title: "Chatfuel Rockets Jersey",
-                  image_url: "https://rockets.chatfuel.com/assets/shirt.jpg",
-                  subtitle: "Size: M",
-                  buttons: [
-                    {
-                      type: "web_url",
-                      url: "https://rockets.chatfuel.com/store",
-                      title: "View Item"
-                    }
-                  ]
+                  "type": "show_block",
+                  "block_names": ["name of block"],
+                  "title": "Show Block"
                 },
                 {
-                  title: "Chatfuel Rockets Jersey",
-                  image_url: "https://rockets.chatfuel.com/assets/shirt.jpg",
-                  subtitle: "Size: L",
-                  default_action: {
-                    type: "web_url",
-                    url: "https://rockets.chatfuel.com/store",
-                    messenger_extensions: true
-                  },
-                  buttons: [
-                    {
-                      type: "web_url",
-                      url: "https://rockets.chatfuel.com/store",
-                      title: "View Item"
-                    }
-                  ]
+                  "type": "web_url",
+                  "url": "https://rockets.chatfuel.com",
+                  "title": "Visit Website"
+                },
+                {
+                  "url": "https://rockets.chatfuel.com/api/welcome",
+                  "type":"json_plugin_url",
+                  "title":"Postback"
                 }
               ]
             }
           }
         }
       ]
-    };
+    }
   };
 
   return fetchJson("https://api.deezer.com/search/artist?q=" + req.query.q)
