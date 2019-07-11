44677773b2cca86ea19e8ea96963dea7347ff382
Ivan Naluzhnyi <ivan.naluzhnyi@gmail.com>
2019-07-11 18:50:39 +0200

    done artiste

--- controllers/artistController.js
+++ controllers/artistController.js
@@ -2,101 +2,44 @@ const fetchJson = require("../services/call-api");
 
 exports.sendArtistsByName = (req, res) => {
   const prepareDataToSend = (response) => {
-    // const prepare = {
-    //   set_attributes: {
-    //     artist_search_events: req.query.q
-    //   },
-    //   messages: [
-    //     {
-    //       attachment: {
-    //         type: 'template',
-    //         payload: {
-    //           template_type: 'generic',
-    //           image_aspect_ratio: 'square',
-    //           elements: []
-    //         }
-    //       }
-    //     }
-    //   ]
-    // };
-
-    // if (response.error) {
-    //   return response.error;
-    // }
-
-    // response.data.forEach(item => {
-    //   prepare.messages[0].attachment.payload.elements.push({
-    //     title: item.name,
-    //     image_url: item.picture_xl,
-    //     subtitle: 'Size: M',
-    //     buttons: [
-    //       {
-    //         type: 'web_url',
-    //         url: item.link,
-    //         title: 'Voir sur deezer'
-    //       }
-    //     ]
-    //   });
-    // });
-
+    console.log('response',response);
     const prepare = {
       messages: [
         {
           attachment: {
-            type: "image",
+            type: "template",
             payload: {
-              url: "https://rockets.chatfuel.com/assets/welcome.png"
+              template_type: "generic",
+              elements: []
             }
           }
         }
       ]
     };
 
-    // return prepare;
-    return {
-      "messages":[
-        {
-          "attachment":{
-            "type":"template",
-            "payload":{
-              "template_type":"generic",
-              "elements":[
-                {
-                  "title":"Get in touch",
-                  "image_url":"https://rockets.chatfuel.com/assets/contact.jpg",
-                  "subtitle":"Feel free to hit us up!",
-                  "buttons":[
-                    {
-                      "type":"phone_number",
-                      "phone_number":"+19268881413",
-                      "title":"Call"
-                    },
-                    {
-                      "type":"element_share"
-                    }
-                  ]
-                },
-                {
-                  "title":"Get in touch",
-                  "image_url":"https://rockets.chatfuel.com/assets/contact.jpg",
-                  "subtitle":"Feel free to hit us up!",
-                  "buttons":[
-                    {
-                      "type":"phone_number",
-                      "phone_number":"+19268881413",
-                      "title":"Call"
-                    },
-                    {
-                      "type":"element_share"
-                    }
-                  ]
-                }
-              ]
-            }
-          }
-        }
-      ]
+    if (response.error) {
+      return response.error;
     }
+
+    response.data.forEach((item) => {
+      prepare.messages[0].attachment.payload.elements.push({
+        title: item.name,
+        image_url: item.picture_xl,
+        subtitle: "Feel free to hit us up!",
+        buttons: [
+          {
+            type: "web_url",
+            url: "item.link",
+            title: "Visit Website"
+          },
+          {
+            type: "element_share"
+          }
+        ]
+      });
+    });
+
+    return prepare;
   };
 
   return fetchJson("https://api.deezer.com/search/artist?q=" + req.query.q)
