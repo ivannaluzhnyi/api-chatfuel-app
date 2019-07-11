3132336fbdcea9e129f325b56ebb933cd0784de9
Ivan Naluzhnyi <ivan.naluzhnyi@gmail.com>
2019-07-11 18:30:55 +0200

    test

--- controllers/artistController.js
+++ controllers/artistController.js
@@ -1,7 +1,7 @@
-const fetchJson = require('../services/call-api');
+const fetchJson = require("../services/call-api");
 
 exports.sendArtistsByName = (req, res) => {
-  const prepareDataToSend = response => {
+  const prepareDataToSend = (response) => {
     const prepare = {
       set_attributes: {
         artist_search_events: req.query.q
@@ -9,10 +9,10 @@ exports.sendArtistsByName = (req, res) => {
       messages: [
         {
           attachment: {
-            type: 'template',
+            type: "template",
             payload: {
-              template_type: 'generic',
-              image_aspect_ratio: 'square',
+              template_type: "generic",
+              image_aspect_ratio: "square",
               elements: []
             }
           }
@@ -24,26 +24,71 @@ exports.sendArtistsByName = (req, res) => {
       return response.error;
     }
 
-    response.data.forEach(item => {
+    response.data.forEach((item) => {
       prepare.messages[0].attachment.payload.elements.push({
         title: item.name,
         image_url: item.picture_xl,
-        subtitle: 'Size: M',
+        subtitle: "Size: M",
         buttons: [
           {
-            type: 'web_url',
+            type: "web_url",
             url: item.link,
-            title: 'Voir sur deezer'
+            title: "Voir sur deezer"
           }
         ]
       });
     });
 
-    return prepare;
+    // return prepare;
+    return {
+      messages: [
+        {
+          attachment: {
+            type: "template",
+            payload: {
+              template_type: "generic",
+              image_aspect_ratio: "square",
+              elements: [
+                {
+                  title: "Chatfuel Rockets Jersey",
+                  image_url: "https://rockets.chatfuel.com/assets/shirt.jpg",
+                  subtitle: "Size: M",
+                  buttons: [
+                    {
+                      type: "web_url",
+                      url: "https://rockets.chatfuel.com/store",
+                      title: "View Item"
+                    }
+                  ]
+                },
+                {
+                  title: "Chatfuel Rockets Jersey",
+                  image_url: "https://rockets.chatfuel.com/assets/shirt.jpg",
+                  subtitle: "Size: L",
+                  default_action: {
+                    type: "web_url",
+                    url: "https://rockets.chatfuel.com/store",
+                    messenger_extensions: true
+                  },
+                  buttons: [
+                    {
+                      type: "web_url",
+                      url: "https://rockets.chatfuel.com/store",
+                      title: "View Item"
+                    }
+                  ]
+                }
+              ]
+            }
+          }
+        }
+      ]
+    };
   };
-  return fetchJson('https://api.deezer.com/search/artist?q=' + req.query.q)
-    .then(data => data.json())
-    .then(response => {
+
+  return fetchJson("https://api.deezer.com/search/artist?q=" + req.query.q)
+    .then((data) => data.json())
+    .then((response) => {
       return res.json(prepareDataToSend(response));
     });
 };
