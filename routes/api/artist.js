f4c47e023beff5ce3735301118c3442b12268ff7
Ivan Naluzhnyi <ivan.naluzhnyi@gmail.com>
2019-07-12 19:47:56 +0200

    song

--- routes/api/artist.js
+++ routes/api/artist.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const songController = require('../../controllers/songController');
 // return a test
-router.get('/', songController.sendSongByName);
+router.get('/', artistController.sendArtistsByName);
 
 module.exports = router;
--- routes/api/song.js
+++ routes/api/song.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const artistController = require('../../controllers/artistController');
 // return a test
-router.get('/', artistController.sendArtistsByName);
+router.get('/',songController.sendSongByName );
 
 module.exports = router;
